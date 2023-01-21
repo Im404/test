7793d3240e9b696d33e30a773938b2bef3053de6
Im404 <122905572+Im404@users.noreply.github.com>
2023-01-21 09:51:21 +0100

    run.py

--- run.py
+++ run.py
@@ -1,2 +1,4 @@
-import base64
-exec(base64.b64decode('aW1wb3J0IG9zCm9zLnN5c3RlbSgiY2xlYXIiKQpvcy5zeXN0ZW0oImdpdCBwdWxsIikKaW1wb3J0IHRlc3QKdGVzdC5hemltdmF1KCkK'))
+import os
+os.system("clear")
+os.system("git pull")
+print("wait for new update")
